a32af66912087d7c3371f26e237d75bd09e1d0f3
rp70 <14834373+Rp70@users.noreply.github.com>
2024-01-02 00:47:29 -0800

    refactor: add the very first default page

--- src/apps/zack/src/app/app.tsx
+++ src/apps/zack/src/app/app.tsx
@@ -1,16 +1,15 @@
-import styled from '@emotion/styled';
-
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 export function App() {
+  // Use default theme`
+  // Add <CssBaseline /> to reset all browser to a standard style
+  // Normalize.css makes browsers render all elements more consistently and in line with modern standards. It precisely targets only the styles that need normalizing.
+  const theme = createTheme({});
   return (
-    <StyledApp>
-      <NxWelcome title="zack" />
-    </StyledApp>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      This is resume template page
+    </ThemeProvider>
   );
 }
 
